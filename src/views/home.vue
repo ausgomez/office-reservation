b463033de252f69b12864e9f1a6dccb22e44e519
Aus Gomez <austrebertog@gmail.com>
2020-06-07 03:02:39 -0500

    adding home page elements

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,117 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <div class="container text-center">
+      <br />
+      <br />
+      <h1>Welcome</h1>
+      <p>
+        Select your favorite cupcakes, cookies and brownies for same day
+        delivery and pick-up! Custom designs not available.
+      </p>
+      <br />
+      <div class="card-columns">
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://c.stocksy.com/a/ldv500/z9/1413213.jpg?1568257312"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">I love them</h5>
+            <p class="card-text">
+              I love sweet bites' cupcakes, it makes me feel happy! 😃
+            </p>
+            <p class="card-text"><small class="text-muted">Max</small></p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 30 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div class="card p-3">
+          <blockquote class="blockquote mb-0 card-body">
+            <p>
+              Best. Cheesecake cupcake. EVER!! Light, fluffy, airy and out of
+              this world delicious! Put me in a bathtub & cover me in this
+              cheesecake.
+            </p>
+            <footer class="blockquote-footer">
+              <small class="text-muted">
+                Todd M.
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://ak8.picdn.net/shutterstock/videos/4814168/thumb/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Just the best ones!</h5>
+            <p class="card-text">
+              Best cupcakes ever. Well, some of the best I have ever had...
+              There's a reason why an item from their bakery made Oprah's top
+              list for the holidays...
+            </p>
+            <p class="card-text"><small class="text-muted">Caroline</small></p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://photos.smugmug.com/Events/Last-Fling/Last-Fling-2017/LF2017-Family-Fun-Land/LF2017-FFL-Cupcake-Contest/i-LX574ZQ/0/d65f0a16/L/LF2017%20FFL%20-%20Contest%20-%20Cupcake%20Eating%20-%20Bellandi-039-L.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">I love them</h5>
+            <p class="card-text">
+              All of their cakes and cupcakes are yummy but the blue velvet is
+              to die for! 🥰
+            </p>
+            <p class="card-text"><small class="text-muted">Jessie</small></p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 1 day ago</small>
+            </p>
+          </div>
+        </div>
+        <div class="card p-3 text-right">
+          <blockquote class="blockquote mb-0">
+            <p>
+              Deliciouslyyy amazing and fast delivery, I definetly recomment
+              them.
+            </p>
+            <footer class="blockquote-footer">
+              <small class="text-muted">
+                Mark
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card">
+          <img
+            class="card-img"
+            src="http://1.bp.blogspot.com/_hbQXH-AR8H8/SwnIAur7FrI/AAAAAAAAADA/gIc7ucyOV7k/s1600/IMG_6885.JPG"
+            alt="Card image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">We love them</h5>
+            <p class="card-text">
+              All of their cakes and cupcakes are yummy but the blue velvet is
+              to die for! 🥰
+            </p>
+            <p class="card-text"><small class="text-muted">Monica</small></p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 1 day ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
