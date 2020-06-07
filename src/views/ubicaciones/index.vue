b3ebfb1869e55b948196e681eba8563e3a96017a
Aus Gomez <austrebertog@gmail.com>
2020-06-07 00:26:54 -0500

    adding new style to ubicaciones index

--- src/views/ubicaciones/index.vue
+++ src/views/ubicaciones/index.vue
@@ -2,38 +2,61 @@
   <v-container fluid class="grey lighten-5">
     <v-row no-gutters>
       <v-col>
+        <h1 class="text-center">Ubicaciones</h1>
         <v-card
           color="grey lighten-4"
           flat
           width="40em"
           tile
-          v-for="k in lista_edificios"
-          :key="k"
+          v-for="ubicacion in ubicaciones"
+          :key="ubicacion.id"
         >
           <v-container fluid>
             <v-row no-gutters>
               <v-col>
-                <v-img
-                  src="https://picsum.photos/510/300?random"
-                  width="40em"
-                ></v-img>
+                <v-card elevation="2">
+                  <v-carousel
+                    :continuous="false"
+                    :cycle="true"
+                    :show-arrows="true"
+                    hide-delimiter-background
+                    delimiter-icon="mdi-minus"
+                    height="250"
+                  >
+                    <v-carousel-item
+                      v-for="(img, i) in ubicacion.imagenes"
+                      :key="i"
+                      :src="img"
+                      reverse-transition="fade-transition"
+                      transition="fade-transition"
+                    ></v-carousel-item>
+                  </v-carousel>
+                </v-card>
               </v-col>
               <v-col>
-                <v-card color="rgb(69, 132, 255)" dark>
+                <v-card color="rgb(69, 132, 255)" height="250" dark>
                   <v-btn class="float-right" icon>
                     <v-icon>mdi-heart</v-icon>
                   </v-btn>
-                  <v-card-title class="headline">Edificio {{ k }}</v-card-title>
+                  <v-card-title class="headline"
+                    >Edificio {{ ubicacion.nombre }}
+                    <v-rating
+                      :value="4.5"
+                      color="white"
+                      dense
+                      half-increments
+                      readonly
+                      size="14"
+                    ></v-rating
+                  ></v-card-title>
 
-                  <v-card-subtitle
-                    >calle no.{{ k }}, colonia aurora,09431</v-card-subtitle
-                  >
+                  <v-card-subtitle>{{ ubicacion.direccion }}</v-card-subtitle>
 
                   <v-card-actions>
                     <router-link
                       :to="{
                         name: 'UbicacionesShow',
-                        params: { ubicacion_id: k.id },
+                        params: { ubicacion_id: ubicacion.id },
                       }"
                     >
                       <v-btn text>ver mas</v-btn></router-link
@@ -48,7 +71,7 @@
       </v-col>
 
       <v-col>
-        <v-card color="grey lighten-4" height="30em" width="31em">
+        <v-card color="grey lighten-4" height="50em" width="31em">
           <v-toolbar dense> </v-toolbar>
           <MapGoogle />
         </v-card>
@@ -61,16 +84,29 @@
 import MapGoogle from "@/components/maps/MapsGoogle.vue";
 
 export default {
-  name: "HelloWorld",
+  name: "UbicacionesIndex",
   components: {
     MapGoogle,
   },
   data: () => ({
-    lista_edificios: [
-      "edificio principal",
-      "edificio este",
-      "edificio sur",
-      "edificio norte",
+    ubicaciones: [
+      {
+        id: 825715,
+        nombre: "Edificio Central",
+        descripcion:
+          "Bacon ipsum dolor amet t-bone tail chicken rump bresaola pig chuck buffalo biltong drumstick flank beef ribs capicola. Kielbasa biltong alcatra pancetta prosciutto drumstick. Pork sausage tri-tip, frankfurter prosciutto doner spare ribs meatloaf shoulder chicken tongue shankle",
+        telefono: "+52 555-5555",
+        empresa_id: 1772,
+        lat: 9,
+        lon: 10,
+        activo: true,
+        direccion: `Calle 13 #456 Colonia Las Milpas, CP 89228 Toluca, Mexico`,
+        imagenes: [
+          "https://locations-api-production.imgix.net/locations/image/f0381cfe-1aba-11ea-97fa-0a5bc5747799/Web_72DPI-20191121_WeWork_Williams_Square_Dallas_015.jpg?auto=format%20compress&fit=crop&q=50&w=900&h=506",
+          "https://locations-api-production.imgix.net/locations/image/ef449688-1aba-11ea-97fa-0a5bc5747799/Web_72DPI-20191121_WeWork_Williams_Square_Dallas_003.jpg?auto=format%20compress&fit=crop&q=50&w=900&h=506",
+          "https://locations-api-production.imgix.net/locations/image/ef53fac4-1aba-11ea-97fa-0a5bc5747799/Web_72DPI-20191121_WeWork_Williams_Square_Dallas_004.jpg?auto=format%20compress&fit=crop&q=50&w=900&h=506",
+        ],
+      },
     ],
   }),
 };
