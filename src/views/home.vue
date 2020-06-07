201faa64cc324db80054a837dd028b82005701a6
nestornia <nestor.nia@gmail.com>
2020-06-07 14:23:03 -0500

    Adding the home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,127 +1,198 @@
 <template>
-  <div>
-    <div class="container text-center">
-      <br />
-      <br />
-      <h1>Welcome</h1>
-      <p>
-        Select your favorite cupcakes, cookies and brownies for same day
-        delivery and pick-up! Custom designs not available.
-      </p>
-      <br />
-      <div class="card-columns">
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://c.stocksy.com/a/ldv500/z9/1413213.jpg?1568257312"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">I love them</h5>
-            <p class="card-text">
-              I love sweet bites' cupcakes, it makes me feel happy! 😃
-            </p>
-            <p class="card-text"><small class="text-muted">Max</small></p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 30 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div class="card p-3">
-          <blockquote class="blockquote mb-0 card-body">
-            <p>
-              Best. Cheesecake cupcake. EVER!! Light, fluffy, airy and out of
-              this world delicious! Put me in a bathtub & cover me in this
-              cheesecake.
-            </p>
-            <footer class="blockquote-footer">
-              <small class="text-muted">
-                Todd M.
-              </small>
-            </footer>
-          </blockquote>
-        </div>
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://ak8.picdn.net/shutterstock/videos/4814168/thumb/1.jpg"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Just the best ones!</h5>
-            <p class="card-text">
-              Best cupcakes ever. Well, some of the best I have ever had...
-              There's a reason why an item from their bakery made Oprah's top
-              list for the holidays...
-            </p>
-            <p class="card-text"><small class="text-muted">Caroline</small></p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://photos.smugmug.com/Events/Last-Fling/Last-Fling-2017/LF2017-Family-Fun-Land/LF2017-FFL-Cupcake-Contest/i-LX574ZQ/0/d65f0a16/L/LF2017%20FFL%20-%20Contest%20-%20Cupcake%20Eating%20-%20Bellandi-039-L.jpg"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">I love them</h5>
-            <p class="card-text">
-              All of their cakes and cupcakes are yummy but the blue velvet is
-              to die for! 🥰
-            </p>
-            <p class="card-text"><small class="text-muted">Jessie</small></p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 1 day ago</small>
-            </p>
-          </div>
-        </div>
-        <div class="card p-3 text-right">
-          <blockquote class="blockquote mb-0">
-            <p>
-              Deliciouslyyy amazing and fast delivery, I definetly recomment
-              them.
-            </p>
-            <footer class="blockquote-footer">
-              <small class="text-muted">
-                Mark
-              </small>
-            </footer>
-          </blockquote>
-        </div>
-        <div class="card">
-          <img
-            class="card-img"
-            src="http://1.bp.blogspot.com/_hbQXH-AR8H8/SwnIAur7FrI/AAAAAAAAADA/gIc7ucyOV7k/s1600/IMG_6885.JPG"
-            alt="Card image"
-          />
-          <div class="card-body">
-            <h5 class="card-title">We love them</h5>
-            <p class="card-text">
-              All of their cakes and cupcakes are yummy but the blue velvet is
-              to die for! 🥰
-            </p>
-            <p class="card-text"><small class="text-muted">Monica</small></p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 1 day ago</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <v-content>
+      <section id="hero">
+        <v-row no-gutters>
+          <v-img
+            :min-height="'calc(50vh - ' + $vuetify.application.top + 'px)'"
+            src="https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          >
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  align="center"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-col
+                    class="white--text text-center"
+                    cols="12"
+                    tag="h1"
+                  >
+                    <span
+                    :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                      class="font-weight-light"
+                    >
+                      Encuentra tu espacio de trabajo en
+                    </span>
+
+                    <br>
+
+                    <span
+                      :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                      class="font-weight-black"
+                    >
+                      Coprotel
+                    </span>
+
+                  </v-col>
+
+                  <v-btn
+                    class="align-self-end"
+                    fab
+                    outlined
+                    @click="$vuetify.goTo('#about-me')"
+                  >
+                    <v-icon>mdi-chevron-double-down</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+          </v-img>
+        </v-row>
+      </section>
+
+      <section id="about-me">
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">Reserva ya</h2>
+
+          <v-responsive
+            class="mx-auto mb-8"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
+          >
+           <v-form ref="form"
+              v-model="valid"
+              lazy-validation
+            >
+            <p>Selecciona tu ciudad</p>
+
+            <v-overflow-btn
+              class="my-2"
+              :items="dropdown_icon"
+              label="Ciudad"
+              target="#dropdown-example"
+            ></v-overflow-btn>
+
+           </v-form>
+          </v-responsive>
+
+          <div></div>
+
+          <v-btn
+            color="grey"
+            href="/empresas/index.vue"
+            outlined
+            large
+          >
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Reservar Ya
+            </span>
+          </v-btn>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="stats">
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+          src="https://images.unsplash.com/photo-1483389127117-b6a2102724ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        >
+          <v-container fill-height>
+            <v-row class="mx-auto">
+              <v-col
+                v-for="[value, title] of stats"
+                :key="title"
+                cols="12"
+                md="4"
+              >
+                <div class="text-center">
+                  <div
+                    class="display-3 font-weight-black mb-4"
+                    v-text="value"
+                  ></div>
+
+                  <div
+                    class="title font-weight-regular text-uppercase"
+                    v-text="title"
+                  ></div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-parallax>
+      </section>
+    </v-content>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 </template>
 
 <script>
 export default {
   name: "home",
-  data: () => ({
-    title: "Office Reservation",
-  }),
-  methods: {},
+  components: {},
+   data () {
+      return {
+        articles: [
+          {
+            src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+            title: 'Mobile first & Responsive',
+            text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
+          },
+          {
+            src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+            title: 'Think outside the box',
+            text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
+          },
+          {
+            src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
+            title: 'Small changes, big difference',
+            text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
+          },
+        ],
+        features: [
+          {
+            icon: 'mdi-account-group-outline',
+            title: 'Vibrant Community',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam',
+          },
+          {
+            icon: 'mdi-update',
+            title: 'Frequent Updates',
+            text: 'Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit.',
+          },
+          {
+            icon: 'mdi-shield-outline',
+            title: 'Long-term Support',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam',
+          },
+        ],
+        stats: [
+          ['Reserva espacios', '+631 disp'],
+          ['Registra ubicaciones', '+143 disponibles'],
+          ['Aceeso con código QR', '+1532 generados'],
+        ],
+        dropdown_icon: [
+        { text: 'Ciudad de México', callback: () => console.log('Ciudad de México') },
+        { text: 'Guadalajara', callback: () => console.log('Guadalajara') },
+        { text: 'Puebla', callback: () => console.log('Puebla') },
+      ],
+      dropdown_edit: [
+        { text: '100%' },
+        { text: '75%' },
+        { text: '50%' },
+        { text: '25%' },
+        { text: '0%' },
+      ],
+      }
+    }
 };
 </script>
-
-<style></style>
